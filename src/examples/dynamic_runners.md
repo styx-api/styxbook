c08259307db424b207391507535655d4f5dbc4f3
Florian Rupprecht <floruppr@gmail.com>
2024-09-17 10:49:51 -0400

    Add dynamic runners example

--- src/examples/dynamic_runners.md
+++ src/examples/dynamic_runners.md
@@ -1,1 +1,8 @@
 # Example: Dynamic runners
+
+A common pattern in processing pipelines with Styx is dynamically choosing what runner Styx should use. This allows the same pipeline to run e.g. both on your local machine for testing as well as on your HPC cluster. 
+
+```Python
+{{#include pysrc/dynamic_runners.py:5:}}
+```
+[Full source.](pysrc/dynamic_runners.py)
---
+++ src/examples/pysrc/dynamic_runners.py
@@ -0,0 +1,21 @@
+from styxdefs import set_global_runner
+from styxdocker import DockerRunner
+from styxsingularity import SingularityRunner
+
+runner_type = "docker"  # You could read this from a CLI argument,
+                        # config file, or check what system you are
+                        # running on.
+
+if runner_type == "docker":
+    print("Using docker runner.")
+    runner = DockerRunner()
+
+elif runner_type == "singularity":
+    print("Using singularity runner.")
+    runner = SingularityRunner()
+
+else:
+    print("Using local runner.")
+    runner = LocalRunner()
+
+set_global_runner(runner)
