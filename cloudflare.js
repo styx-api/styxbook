017e5fa4fac82b1bafb306659c9f90daa10528fe
Jason Kai <21226986+kaitj@users.noreply.github.com>
2025-12-03 16:38:19 -0500

    Add cloudflare analytics js

---
+++ cloudflare.js
@@ -0,0 +1,8 @@
+// Cloudflare Web Analytics
+(function() {
+    var script = document.createElement("script");
+    script.defer = true;
+    script.src = "https://static.cloudflareinsights.com/beacon.min.js";
+    script.setAttribute('data-cf-beacon', '{"token": "3cc81275d9484412b3569fdb3580758a"}');
+    document.head.appendChild(script);
+})();
